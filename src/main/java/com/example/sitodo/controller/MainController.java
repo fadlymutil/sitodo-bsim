f07ff626e0a23e126ec22ad93ecf43e6076551a6
Fadly M <fadlymutil@gmail.com>
2023-12-12 16:04:10 +0700

    Update MainController.java

--- src/main/java/com/example/sitodo/controller/MainController.java
+++ src/main/java/com/example/sitodo/controller/MainController.java
@@ -1,7 +1,7 @@
 package com.example.sitodo.controller;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping; --code smell
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
